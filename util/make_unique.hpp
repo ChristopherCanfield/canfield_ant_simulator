df4ac106693755cd87fe23868537c503a2f182ea
Christopher D. Canfield <cdcanfield@gmail.com>
2013-11-11 01:19:31 -0500

    Converted spaces to tabs in make_unique

--- util/make_unique.hpp
+++ util/make_unique.hpp
@@ -14,25 +14,25 @@
 template<class T, class Arg1>
 std::unique_ptr<T> make_unique(Arg1&& arg1)
 {
-    return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1)));
+	return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1)));
 }
 
 template<class T, class Arg1, class Arg2>
 std::unique_ptr<T> make_unique(Arg1&& arg1, Arg2&& arg2)
 {
-    return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2)));
+	return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2)));
 }
 
 template<class T, class Arg1, class Arg2, class Arg3>
 std::unique_ptr<T> make_unique(Arg1&& arg1, Arg2&& arg2, Arg3&& arg3)
 {
-    return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2),
+	return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2),
 									std::forward<Arg3>(arg3)));
 }
 
 template<class T, class Arg1, class Arg2, class Arg3, class Arg4>
 std::unique_ptr<T> make_unique(Arg1&& arg1, Arg2&& arg2, Arg3&& arg3, Arg4&& arg4)
 {
-    return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2),
+	return std::unique_ptr<T>(new T(std::forward<Arg1>(arg1), std::forward<Arg2>(arg2),
 									std::forward<Arg3>(arg3), std::forward<Arg4>(arg4)));
 }
